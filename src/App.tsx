9a567cd312ac93f0b1b52983cb09ba643278dc12
artchristech <mandalazenwave@gmail.com>
2025-07-10 08:21:19 -0600

    Add fade-in animation on load

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ChatInterface } from './components/ChatInterface';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900">
+    <div className="min-h-screen bg-gray-900 animate-fade-in">
       <ChatInterface />
     </div>
   );
