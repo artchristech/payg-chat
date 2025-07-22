ae876fb9da565f751c9b9f969003aed117b47e44
artchristech <mandalazenwave@gmail.com>
2025-07-21 19:37:58 -0600

    Fix useState import error in ChatInterface

--- src/components/ChatInterface.tsx
+++ src/components/ChatInterface.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { MessageBubble } from './MessageBubble';
 import { InputArea } from './InputArea';
 import { PresetButtons } from './PresetButtons';
